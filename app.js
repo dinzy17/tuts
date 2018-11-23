75f568c0545e373ccc91d94dc1a8bc8e9d0daeca
Dinesh <dineshnarendrapatil@gmail.com>
2018-11-23 18:18:47 +0530

    making it simple to check

--- app.js
+++ app.js
@@ -3,22 +3,19 @@ const app = express()
 const http = require('http')
 const https = require('https')
 
-const router = require('./')
-const mongooseConnect = require('./helpers/database')
-
-app.use('/', router)
-mongooseConnect.dbConnect()
-
-
-
 var httpsOptions = {
-  key: fs.readFileSync('benchpoint.key'),
-  cert: fs.readFileSync('benchpoint.crt')
+  key: fs.readFileSync('your_ssl_key_file.key'),
+  cert: fs.readFileSync('your_ssl_crt_file.crt')
 }
+
+app.get('/', (req, res) => {
+  res.send("Hello world")
+})
 
 const sslServer = https.createServer(httpsOptions, app).listen(443, () => {
   console.log('https server running at ' + 443)
 })
+
  const server = http.createServer(app).listen(80, () => {
    console.log('http server running at ' + 80)
  })
--- routers.js
+++ routers.js
@@ -1,10 +1,9 @@
 var express = require('express')
-var router = express.Router()
-// Using Node.js `require()`
-const mongoose = require('mongoose');
+var router = express.Router();
 
 router.get("/",	function(req,res){
 	res.send("Hello World")
 })
 
-module.exports = router
+
+module.exports = router;
