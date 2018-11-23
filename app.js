4bd628e46f2a0316086e5c9ad7d9afb4a3c17afd
Dinesh <dineshnarendrapatil@gmail.com>
2018-11-23 18:13:47 +0530

    changing port

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const http = require('http')
+const https = require('https')
+
 const router = require('./')
 const mongooseConnect = require('./helpers/database')
 
 app.use('/', router)
-
 mongooseConnect.dbConnect()
 
 
-var server = app.listen(3000, function () {
-   console.log("Example app listening at http://localhost:3000")
+
+var httpsOptions = {
+  key: fs.readFileSync('benchpoint.key'),
+  cert: fs.readFileSync('benchpoint.crt')
+}
+
+const sslServer = https.createServer(httpsOptions, app).listen(443, () => {
+  console.log('https server running at ' + 443)
 })
+ const server = http.createServer(app).listen(80, () => {
+   console.log('http server running at ' + 80)
+ })
