21ff9bcdec6c97d8920beaa9ac03d365924e3111
nilu4179 <nileshy@arkenea.com>
2018-11-28 15:45:46 +0530

    commenting https code

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const express = require('express')
 const app = express()
+
 const fs = require('fs')
 const http = require('http')
-const https = require('https')
+//const https = require('https')
 
-const httpsOptions = { }
+//const httpsOptions = {
+//  key: fs.readFileSync('your_ssl_key_file.key'),
+//  cert: fs.readFileSync('your_ssl_crt_file.crt')
+//}
 
-// const httpsOptions = {
-//   key: fs.readFileSync('your_ssl_key_file.key'),
-//   cert: fs.readFileSync('your_ssl_crt_file.crt')
-// }
 
-app.get('/', (req, res) => {
-  res.send("Hello world !!!!")
-})
-
-const sslServer = https.createServer(httpsOptions, app).listen(443, () => {
-  console.log('https server running at ' + 443)
-})
-
+//const sslServer = https.createServer(httpsOptions, app).listen(443, () => {
+//  console.log('https server running at ' + 443)
+//})
  const server = http.createServer(app).listen(80, () => {
    console.log('http server running at ' + 80)
  })
